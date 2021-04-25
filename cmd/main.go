666dbba63740d60cc65cdb551e27a04fbd788123
echenim <myron.echenim@gmail.com>
2021-04-25 02:16:37 -0700

    create command constants

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+const READ string = "READ"
+const WRITE string = "WRITE"
+const DELETE string = "DELETE"
+const START string = "START"
+const COMMIT string = "COMMIT"
+const ABORT string = "ABORT"
+const QUIT string = "QUIT"
+
+func main() {
+
+}
