a264a383f7d62f920c2ba01570eed810140cba9b
echenim <myron.echenim@gmail.com>
2021-04-25 02:18:43 -0700

    load in functuoins

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const READ string = "READ"
 const WRITE string = "WRITE"
 const DELETE string = "DELETE"
@@ -9,5 +11,18 @@ const ABORT string = "ABORT"
 const QUIT string = "QUIT"
 
 func main() {
+	header()
+}
 
+func header() {
+
+	fmt.Println("solawind-repl> List of commands: ")
+	fmt.Println("solawind-repl> WRITE     - Stores val in key. ")
+	fmt.Println("solawind-repl> START     - Start a transaction ")
+	fmt.Println("solawind-repl> COMMIT    - Commit a transaction ")
+	fmt.Println("solawind-repl> DELETE    - Removes a key from store ")
+	fmt.Println("solawind-repl> CLS       - Clear the Terminal Screen ")
+	fmt.Println("solawind-repl> READ      - Reads and prints, to stdout, the val associated with key ")
+	fmt.Println("solawind-repl> ABORT     - Abort a transaction. All actions in the current transaction are discarded ")
+	fmt.Println("solawind-repl> QUIT      - Exit the REPL cleanly")
 }
