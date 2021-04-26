6d3b81ef52d2a2e404bfe5001db6cc1ae39beb0c
echenim <myron.echenim@gmail.com>
2021-04-26 14:21:38 -0700

    testing

--- services/sola-wind-repl_test.go
+++ services/sola-wind-repl_test.go
@@ -1,21 +1,54 @@
 package services
 
-func TestWrite() {
+import (
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIfStartTransactionIsNotEnabled(t *testing.T) {
+	sw := SolaWind{}
+	sw.IsTransaction = false
+	sw.Store = map[string]string{}
+	sw.Transactions = map[string]string{}
+	assert.Falsef(t, sw.IsTransaction, "Transaction is not enabled!")
+	sw.Start()
+	assert.False(t, sw.IsTransaction, "Transaction is enabled by the command Start!")
 }
 
-func TestRead() {
-
+func TestIfTransactionIsEnabled(t *testing.T) {
+	sw := SolaWind{}
+	sw.IsTransaction = false
+	sw.Store = map[string]string{}
+	sw.Transactions = map[string]string{}
+	assert.True(t, sw.IsTransaction, "Transaction is enabled by the command Start!")
 }
 
-func TestDelete() {
-
+func TestIfTransactionIsEnabledByStartCmd(t *testing.T) {
+	sw := SolaWind{}
+	sw.IsTransaction = false
+	sw.Store = map[string]string{}
+	sw.Transactions = map[string]string{}
+	sw.Start()
+	assert.False(t, sw.IsTransaction, "Transaction is enabled by the command Start!")
 }
 
-func TestCommit() {
-
+func TestCommitIfTransactionIsOn(t *testing.T) {
+	sw := SolaWind{}
+	sw.IsTransaction = true
+	sw.Store = map[string]string{}
+	sw.Transactions = map[string]string{}
+	if assert.True(t, sw.IsTransaction) {
+		sw.Commit()
+	}
 }
 
-func TestAbort() {
-
+func TestCommitIfTransactionIsNotOn(t *testing.T) {
+	sw := SolaWind{}
+	sw.IsTransaction = false
+	sw.Store = map[string]string{}
+	sw.Transactions = map[string]string{}
+	if assert.True(t, sw.IsTransaction, "There is no transaction to commit") {
+		sw.Commit()
+	}
 }
