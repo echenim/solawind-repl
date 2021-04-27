1cf96f429a8a45a754f882dc1452d603395a47c4
echenim <myron.echenim@gmail.com>
2021-04-27 09:52:18 -0700

    readme modification

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# solawind-repl
+# SOLAWIND REPL
+
+## Commands
+```zsh
+WRITE     - Stores val in key. 
+START     - Start a transaction 
+COMMIT    - Commit a transaction 
+DELETE    - Removes a key from store 
+READ      - Reads and prints, to stdout, the val associated with key 
+ABORT     - Abort a transaction. All actions in the current transaction are discarded 
+QUIT      - Exit the REPL cleanly
+```
--- cmd/main.go
+++ cmd/main.go
@@ -36,7 +36,6 @@ func header() {
 	fmt.Println("solawind-repl> START     - Start a transaction ")
 	fmt.Println("solawind-repl> COMMIT    - Commit a transaction ")
 	fmt.Println("solawind-repl> DELETE    - Removes a key from store ")
-	fmt.Println("solawind-repl> CLS       - Clear the Terminal Screen ")
 	fmt.Println("solawind-repl> READ      - Reads and prints, to stdout, the val associated with key ")
 	fmt.Println("solawind-repl> ABORT     - Abort a transaction. All actions in the current transaction are discarded ")
 	fmt.Println("solawind-repl> QUIT      - Exit the REPL cleanly")
